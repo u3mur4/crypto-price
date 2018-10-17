5f3663cb0239f1a158ec61eca8cc2d383a66d3e6
Kenshou Uemura <e3mur4@gmail.com>
2018-10-17 10:56:08 +0200

    Add version flag

--- cmd/root.go
+++ cmd/root.go
@@ -54,7 +54,9 @@ var rootCmd = &cobra.Command{
 
 // Execute adds all child commands to the root command and sets flags appropriately.
 // This is called by main.main(). It only needs to happen once to the rootCmd.
-func Execute() {
+func Execute(version, commit, date string) {
+	rootCmd.Version = fmt.Sprintf("%v, commit %v, built at %v", version, commit, date)
+
 	if err := rootCmd.Execute(); err != nil {
 		fmt.Println(err)
 		os.Exit(1)
--- main.go
+++ main.go
@@ -1,7 +1,15 @@
 package main
 
-import "github.com/u3mur4/crypto-price/cmd"
+import (
+	"github.com/u3mur4/crypto-price/cmd"
+)
+
+var (
+	version = "dev"
+	commit  = "none"
+	date    = "unknown"
+)
 
 func main() {
-	cmd.Execute()
+	cmd.Execute(version, commit, date)
 }
